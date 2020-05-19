d58a538f357b2920820de6308f4d495409ffb493
AnthonyLedesma <anthonymledesma@gmail.com>
2020-05-19 15:56:12 -0700

    try fixes

--- src/blocks/media-card/test/media-card.cypress.js
+++ src/blocks/media-card/test/media-card.cypress.js
@@ -44,7 +44,7 @@ describe( 'Test CoBlocks Media Card Block', function() {
 
 		helpers.upload.imageToBlock( 'coblocks/media-card' );
 
-		cy.get( '.wp-block-coblocks-media-card img[src*="http"]' )
+		cy.get( 'figure.wp-block-coblocks-media-card__media-container img[src*="http"]' )
 			.should( 'have.attr', 'src' )
 			.should( 'include', imageBase );
 
